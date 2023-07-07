151e565b7174fd3fa282b19b23548e0cfb1222c5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-07 09:34:56 -0300

    Aula 94 - Navegação entre Componentes, curso React

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 export default function Inicio() {
   //'<h1>Título</h1>'
   return (
     <div>
-      <h1>Fundamentos de Next.JS & React</h1>
-      <h2>Vamos estudar esse framework</h2>
+      <Link href='/estiloso'>
+        Estiloso
+      </Link>
     </div>
   )
 } 
