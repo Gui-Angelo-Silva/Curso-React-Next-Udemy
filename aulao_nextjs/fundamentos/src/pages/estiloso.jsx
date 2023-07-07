a56933fb1959d94e60a7d86fb7ab65904a2961d5
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-07 11:50:20 -0300

    Aula 95 - Componente Layout, curso React

--- aulao_nextjs/fundamentos/src/pages/estiloso.jsx
+++ aulao_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -1,11 +1,13 @@
+import Layout from '@/components/layout'
 import styles from '../styles/estiloso.module.css'
 import Link from 'next/link'
 
 export default function Estiloso() {
     return (
-        <div className= { styles.roxo }>
-            <Link href='/'> Voltar</Link>
-            <h1>Estilo usando CSS Módulos</h1>
-        </div>
+        <Layout titulo='Exemplo de CSS Modularizado'>
+            <div className={styles.roxo}>
+                <h1>Estilo usando CSS Módulos</h1>
+            </div>
+        </Layout>
     )
 }
