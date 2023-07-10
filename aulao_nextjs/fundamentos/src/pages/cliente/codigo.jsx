810a0c51dede3234f321ed541a5032b2cb076eff
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-10 11:53:40 -0300

    Continuar

---
+++ aulao_nextjs/fundamentos/src/pages/cliente/codigo.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function CLienteProCodigo() {
+    return (
+        <Layout titulo='Navegação Dinâmica'>
+            <span>Codigo = ???</span>
+        </Layout>
+    )
+}
