88ee7aba3ac127b9046b21f4f29faada008abcd1
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-10 09:25:54 -0300

    Aula 97 - Navegação Simples, Curso React

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function Inicio() {
       <Navegador texto='Estiloso' destino='/estiloso'/>
       <Navegador texto ='Exemplo' destino='/exemplo' cor='#9400d3'/>
       <Navegador texto='JSX' destino='/jsx' cor='crimson'/>
+      <Navegador texto='Navegação #01' destino='/navegacao/' cor='green'/>
     </div>
   )
 } 
