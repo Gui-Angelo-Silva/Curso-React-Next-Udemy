ec9e3ef17529fa81a802777f55c44a9b513d5a4a
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-13 11:44:30 -0300

    110 - Componentes Layout e Titulo, curso React

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-      
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   )
 }
