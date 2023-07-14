8c3ccda629f2680c2364c30ee5190b347963eb24
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-07-14 10:17:20 -0300

    114 - Componente Tabela #03, curso React

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -10,6 +10,14 @@ export default function Home() {
     new Cliente('Gustavo', 12, '4'),
   ]
   
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir...${cliente.nome}`)
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -17,7 +25,10 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+          ></Tabela>
       </Layout>
     </div>
   )
